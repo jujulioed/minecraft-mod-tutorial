fae5073b0dfc1fa014027fdf5f34f0b658718e48
juujulio <julioeduardon9@hotmail.com>
2023-12-10 23:58:26 -0300

    Creating a custom Tool!
     - Created a Metal Detector tool (1/2).
    
    Signed-off-by: juujulio <julioeduardon9@hotmail.com>

--- src/main/java/net/jujulioed/tutorialmod/item/ModCreativeModTabs.java
+++ src/main/java/net/jujulioed/tutorialmod/item/ModCreativeModTabs.java
@@ -22,6 +22,8 @@ public class ModCreativeModTabs {
                         pOutput.accept(ModItems.SAPHIRE.get());
                         pOutput.accept(ModItems.RAW_SAPHIRE.get());
 
+                        pOutput.accept(ModItems.METAL_DETECTOR.get());
+
                         pOutput.accept(ModBlocks.SAPPHIRE_BLOCK.get());
                         pOutput.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
                         pOutput.accept(ModBlocks.SAPPHIRE_ORE.get());
--- src/main/java/net/jujulioed/tutorialmod/item/ModItems.java
+++ src/main/java/net/jujulioed/tutorialmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.jujulioed.tutorialmod.item;
 
 import net.jujulioed.tutorialmod.TutorialMod;
+import net.jujulioed.tutorialmod.item.custom.MetalDetectorItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -11,8 +12,13 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
 
-    public static final RegistryObject<Item> SAPHIRE = ITEMS.register("sapphire", () -> new Item(new Item.Properties()));
-    public static final RegistryObject<Item> RAW_SAPHIRE = ITEMS.register("raw_sapphire", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> SAPHIRE = ITEMS.register("sapphire",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_SAPHIRE = ITEMS.register("raw_sapphire",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> METAL_DETECTOR = ITEMS.register("metal_detector",
+            () -> new MetalDetectorItem(new Item.Properties().durability(100)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
